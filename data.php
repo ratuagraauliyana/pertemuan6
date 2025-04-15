c235723a33552a867e08b94533ac875cdb8dd41a
ratuagraauliyana <2310631250103@student.unsika.ac.id>
2025-04-15 23:35:43 +0700

    Create data.php

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+$bandara_asal = [
+    "Soekarno Hatta" => 65000,
+    "Husein Sastranegara" => 50000,
+    "Abdul Rachman Saleh" => 40000,
+    "Juanda" => 30000
+];
+
+$bandara_tujuan = [
+    "Ngurah Rai" => 85000,
+    "Hasanuddin" => 70000,
+    "Inanwatan" => 90000,
+    "Sultan Iskandar Muda" => 60000
+];
+
+ksort($bandara_asal);
+ksort($bandara_tujuan);
+?>
